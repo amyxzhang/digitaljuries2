bd413561c7a03da639be90943eacab765a18a137
Jenny Fan <fan.jenny@gmail.com>
2019-03-26 11:20:54 -0400

    Update survey styles, instructions

--- juries/static/juries/js/main.js
+++ juries/static/juries/js/main.js
@@ -24,6 +24,12 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
+// Show selected btn
+$('[data-toggle="button"]').click(function() {
+  $(this).button('toggle');
+});
+
+
 // Popover
 $(function () {
   $('[data-toggle="popover"]').popover({
@@ -58,4 +64,3 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-	
